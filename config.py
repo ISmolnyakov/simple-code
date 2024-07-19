989e116ff576eec3925332ea27902d65d01d975b
Игорь Смольняков <91562953+ISmolnyakov@users.noreply.github.com>
2024-07-19 17:32:48 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+bot_token = '6801650600:AAGf0QJ2qI8IHYKEBEx4lQHWc_yUcsCtL5c'
+
+photo_url = 'https://image.lexica.art/full_webp/d7f2c466-827b-4443-8a91-6633d3f8a78b'
+spaceman_pic_url = ('https://images.rawpixel.com/image_png_social_square/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8y'
+                'MDIzLTExL3Jhd3BpeGVsb2ZmaWNlMTFfYXN0cm9uYXV0X19yaXNvZ3JhcGhfc3R5bGVfaXNvbGF0ZWRfb25fd2hpdGVfYl9lZDcwO'
+                'DBjMS0zMjRmLTQ2MmUtOTU5MS0zNzYxYTI1ZTBhZWYucG5n.png')
+
+spaceman_pic_file = 'pics/Снимок экрана 2024-07-17 161130.png'
+
+chat_admin = [
+    # 404888777,
+55541]
+
+content_types = {
+    "text": 'текст',
+    "photo": 'фото',
+    "document": 'документ',
+    "sticker": 'стикер'
+}
